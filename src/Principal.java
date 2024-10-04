b9293c74a0be56d8c22f1f2ec2daaea6e1a58cf1
Manuelser25 <manuelserrano25@gmail.com>
2024-10-04 16:10:47 -0300

    agregando menu de opciones

---
+++ src/Principal.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("*********************************************************************");
+        System.out.println("******BIENVENIDOS AL CONVERSOR DE MONEDAS****************************");
+        System.out.println("*** 1. Dolares a ==> Pesos Argentinos                             ***");
+        System.out.println("*** 2. Pesos Argentinos a ==> Dolares                             ***");
+        System.out.println("*** 3. Dolares a ==> Reales Brasileños                            ***");
+        System.out.println("*** 4. Reales Brasileños a ==> Dolares                            ***");
+        System.out.println("*** 5. Dolares a ==> Pesos Colombianos                            ***");
+        System.out.println("*** 6. Pesos Colombianos a ==> Dolares                            ***");
+        System.out.println("*** 7. Salir                                                      ***");
+        System.out.println("Elija una opción válida");
+        System.out.println("*********************************************************************");
+    }
+}
